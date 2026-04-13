b04b18fc424a688b127f8027ad3ad5554ea8cdcc
Ruchir Sharma <ruchirsharma074@gmail.com>
2026-04-13 16:19:47 +0530

    feat: implement core worker logic with blocking list pop

---
+++ src/workers/index.js
@@ -0,0 +1,30 @@
+import redis from "../config/redis.js";
+
+async function startWorker() {
+    console.log("Worker started!");
+    while (true) {
+        try {
+            const result = await redis.blpop("main-queue", 0);
+            if (result) {
+                const [queueName, data] = result;
+                const job = JSON.parse(data);
+                console.log(`Received job: ${job.id} | Type: ${job.jobType}`);
+                console.log(`Processing from ${queueName}: ${data}`);
+
+                //To simulate actual worker doing work
+                await new Promise(resolve => setTimeout(resolve, 3000));
+                job.status = "Completed";
+
+                console.log(`Successfully processed Job ID: ${job.id} (Status: ${job.status})`);
+            }
+
+        } catch (error) {
+            console.error("Redis Error:", error);
+            //Short sleep to simulate error resolution
+            await new Promise(resolve => setTimeout(resolve, 1000));
+
+        }
+    }
+
+}
+startWorker();
